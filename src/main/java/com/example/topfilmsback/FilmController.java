765cae5acec0fa334c8a3f868ad32b154f0d98f0
x381 <coolonepiecee@gmail.com>
2021-03-18 17:31:39 +0100

    ajout requete GET filmById

--- src/main/java/com/example/topfilmsback/FilmController.java
+++ src/main/java/com/example/topfilmsback/FilmController.java
@@ -5,6 +5,7 @@ import com.example.topfilmsback.service.FilmService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +25,11 @@ public class FilmController {
         List<Film> films = filmService.findAllFilms();
         return new ResponseEntity<>(films, HttpStatus.OK);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Film> getFilm(@PathVariable("id") Long id) {
+        Film film = filmService.findFilmById(id);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
 }
--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -9,3 +9,7 @@ CREATE TABLE film
     duree int,
     primary key (id)
 );
+
+insert into film values (1, 'test', '2020-02-02', 'simon', 192);
+insert into film values (2, 'test', '2020-02-02', 'arthur', 180);
+insert into film values (3, 'test', '2020-02-02', 'remy', 122);
